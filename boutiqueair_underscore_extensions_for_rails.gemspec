de35367a0e6f04648b474b31f3d64c0099c88473
George Mendoza <gsmendoza@gmail.com>
2012-06-22 11:54:04 +0800

    Add test to ensure that assets/boutiqueair_underscore_extensions.js will find the file.

--- boutiqueair_underscore_extensions_for_rails.gemspec
+++ boutiqueair_underscore_extensions_for_rails.gemspec
@@ -19,5 +19,7 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 3.1.6"
   s.add_dependency "underscore-rails"
 
+  s.add_development_dependency 'capybara'
+  s.add_development_dependency 'minitest'
   s.add_development_dependency "sqlite3"
 end
--- test/boutiqueair_underscore_extensions_for_rails_test.rb
+++ test/boutiqueair_underscore_extensions_for_rails_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
-class BoutiqueairUnderscoreExtensionsForRailsTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, BoutiqueairUnderscoreExtensionsForRails
+describe "with static asset integration," do
+  include Capybara::DSL
+
+  describe "boutiqueair_underscore_extensions.js" do
+    it "should be integrated to the asset pipeline" do
+      visit '/assets/boutiqueair_underscore_extensions.js'
+      page.text.must_include 'BoutiqueAirUnderscoreExtensions = {'
+    end
   end
 end
